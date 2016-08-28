cd8b9691ba4d4d7d59242aed0b840a056a013875
LyubozarMihovski <lyubozar@yahoo.com>
2016-08-28 09:56:22 +0300

    Read More

--- DemoBlog/Global.asax.cs
+++ DemoBlog/Global.asax.cs
@@ -19,6 +19,14 @@ namespace DemoBlog
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Application["Totaluser"] = 0;
+
+        }
+        protected void Session_Start()
+        {
+            Application.Lock();
+            Application["Totaluser"] = (int)Application["Totaluser"] + 1;
+            Application.UnLock();
         }
     }
 }
